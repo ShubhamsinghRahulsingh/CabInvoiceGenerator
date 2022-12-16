0d863b2724b3e98de36f7df4c32cae147b6a958c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-16 17:39:27 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ Cab_Invoice_GeneratorTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Cab_Invoice_GeneratorTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
