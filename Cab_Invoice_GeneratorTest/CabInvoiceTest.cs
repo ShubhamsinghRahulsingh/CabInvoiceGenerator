012555b3e5651de4ba3de0892aa6b802725f2bab
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-17 12:05:23 +0530

    (Shubham)Add: Calculating Enhanced Invoice Summary for a given UserID

--- Cab_Invoice_GeneratorTest/CabInvoiceTest.cs
+++ Cab_Invoice_GeneratorTest/CabInvoiceTest.cs
@@ -37,9 +37,29 @@ namespace Cab_Invoice_GeneratorTest
         //Test to get aggregate fare and no of rides for multiple rides after refactor
         public void GivenProperDistanceAndTimeForMultipleRide_ShouldReturnAggregateFare()
         {
-            Ride[] ride = { new Ride(3.0, 5.0), new Ride(2.0, 5.0), new Ride(0.1, 0.5) };
+            Ride[] ride = { new Ride(4.0, 5.0), new Ride(2.0, 5.0), new Ride(0.1, 0.5) };
             EnhancedInvoiceSummary INVOICE_SUMMARY = this.cabInvoiceGenerator.GetMultipleRideFare(ride);
-            Assert.AreEqual(65, INVOICE_SUMMARY.totalFare);
+            Assert.AreEqual(75, INVOICE_SUMMARY.totalFare);
+            Assert.AreEqual(3, INVOICE_SUMMARY.totalNumberOfRides);
+            Assert.AreEqual(25, INVOICE_SUMMARY.averageFarePerRide);
+        }
+        [Test]
+        //Test  List of rides by giving userID
+        public void GivenUserIDInInvoice_GetsListofRides_ReturnAverageFareTotalFare()
+        {
+            CabInvoiceGenerator repository = new CabInvoiceGenerator();
+            Ride[] ride = { new Ride(4.0, 5.0), new Ride(2.0, 5.0), new Ride(0.1, 0.5) };
+            Ride[] rideNew = { new Ride(6.0, 5.0), new Ride(4.0, 5.0) };
+            repository.MapRidesToUser("shubham",ride);
+            repository.MapRidesToUser("rahul", rideNew);
+            EnhancedInvoiceSummary INVOICE_SUMMARY = repository.GetEnhancedInvoiceSummary("shubham");
+            EnhancedInvoiceSummary INVOICE_SUMMARY_NEW = repository.GetEnhancedInvoiceSummary("rahul");
+            Assert.AreEqual(75, INVOICE_SUMMARY.totalFare);
+            Assert.AreEqual(25, INVOICE_SUMMARY.averageFarePerRide);
+            Assert.AreEqual(3, INVOICE_SUMMARY.totalNumberOfRides);
+            Assert.AreEqual(110, INVOICE_SUMMARY_NEW.totalFare);
+            Assert.AreEqual(55, INVOICE_SUMMARY_NEW.averageFarePerRide);
+            Assert.AreEqual(2, INVOICE_SUMMARY_NEW.totalNumberOfRides);
         }
     }
 }
